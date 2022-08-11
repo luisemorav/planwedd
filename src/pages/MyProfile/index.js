8ffb48fd97bfb77d08b5098eb020ea23eff66fc8
luisemorav <luisemorav@hotmail.com>
2022-08-11 17:54:57 -0500

    Profile culminado, carpeta components

--- src/pages/MyProfile/index.js
+++ src/pages/MyProfile/index.js
@@ -1,62 +1,30 @@
+import { Link } from "react-router-dom";
+import ProfileForm from "../../components/ProfileForm";
+
 const MyProfile = () => {
   return (
     <div className="backgroundImage1">
-      <div className="container text-center">
-        <div className="row mt-3">
-          <h1 className="h3">Mi Perfil</h1>
+      <div className="row justify-content-center">
+        <div className="row mt-3 justify-content-center">
+          <h2 className="h3 text-center">Mi Perfil</h2>
         </div>
-        <div>
+        <div className="row img-profile justify-content-center">
           <img
-            src="https://www.seekpng.com/png/detail/966-9665493_my-profile-icon-blank-profile-image-circle.png"
+            src="https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg"
             className="img-fluid"
             alt="ProfImg"
-            width={200}
           />
         </div>
       </div>
-      <div className="row gx-5 justify-content-center">
-        <div className="col-4">
-          <div className="mb-3 row">
-            <label className="col-form-label">Nombre</label>
-            <input type="text" className="form-control" id="txtNombre" />
-          </div>
-        </div>
-        <div className="col-4">
-          <div className="mb-3 row">
-            <label className="col-form-label">Apellido</label>
-            <input type="text" className="form-control" id="txtApellido" />
-          </div>
-        </div>
-      </div>
-      <div className="row gx-5 justify-content-center">
-        <div className="col-4">
-          <div className="mb-3 row">
-            <label className="col-form-label">Mail</label>
-            <input type="email" className="form-control" id="txtNombre" />
-          </div>
-        </div>
-        <div className="col-4">
-          <div className="mb-3 row">
-            <label className="col-form-label">Teléfono</label>
-            <input type="text" className="form-control" id="txtApellido" />
-          </div>
-        </div>
-      </div>
-      <div className="row gx-5 justify-content-center">
-        <div className="col-4">
-          <label className="col-form-label">Tipo de Documento</label>
-          <select class="form-select" aria-label="Default select example">
-            <option selected>DNI</option>
-            <option value="1">Pasaporte</option>
-            <option value="2">Carnet de extranjería</option>
-            <option value="3">RUC</option>
-          </select>
-        </div>
-        <div className="col-4">
-          <div className="mb-3 row">
-            <label className="col-form-label">Número de Documento</label>
-            <input type="text" className="form-control" id="txtApellido" />
-          </div>
+      <ProfileForm />
+      <div className="row justify-content-center mt-3">
+        <div className="col-3 text-center">
+          <Link to="/" className="btn btn-first mx-2">
+            Guardar cambios
+          </Link>
+          <Link to="/myevent" className="btn btn-first mx-2">
+            Mi Evento
+          </Link>
         </div>
       </div>
     </div>
