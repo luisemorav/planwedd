92e14b95680b206d042ba174a1fe8ffe3530b076
luisemorav <luisemorav@hotmail.com>
2022-08-13 10:13:29 -0500

    Correccion commit previo

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,81 +1,5 @@
 import { Link } from "react-router-dom";
 const Register = () => {
-<<<<<<< HEAD
-    return(
-        <div className='backgroundImage2'>
-            <div className="container" style={{maxWidth:'700px', height:'100vh', display:'flex', flexDirection:'column', justifyContent:'center'}} >
-                <div className="container card p-5" style={{background:'rgba(255, 255, 255, 0.25)',backdropFilter:'blur(10px)'}}>
-                    <div className="container" style={{display:'flex', flexDirection:'column',textAlign:'center'}}>
-                        <h1 style={{fontSize:'3em'}}>PLANWEED</h1>
-                        <p style={{fontSize:'2em'}}>Bienvenido!</p>
-                        <p style={{fontSize:'1em'}}>registrate, o si ya tienes una cuenta <Link to={'/login'}>Inicia sesion</Link></p>
-                    </div>
-                    <div className="container mt-5">
-                        <form action="" className="d-flex flex-column gap-3">
-                            <input 
-                            className="form-control p-3" 
-                            type="text" 
-                            placeholder="nombre de usuario"
-                            style={{background:'transparent',border:'2px solid var(--button-color)'}}
-                            />
-                            <input 
-                            className="form-control p-3" 
-                            type="email" 
-                            placeholder="email"
-                            style={{background:'transparent',border:'2px solid var(--button-color)'}}
-                            />
-                            <div class="input-group">
-                                <input 
-                                type="text" 
-                                className="form-control p-3" 
-                                placeholder="contrasenia" 
-                                aria-label="Recipient's username" 
-                                aria-describedby="button-addon2"
-                                style={{background:'transparent',border:'2px solid var(--button-color)'}} 
-                                />
-                                <button class="btn btn-outline-success" type="button" id="button-addon2" style={{border:'2px solid var(--button-color)',color:'var(--button-color)'}}><i class="fa-solid fa-eye"></i></button>
-                            </div>
-                            <div class="input-group">
-                                <input 
-                                type="text" 
-                                className="form-control p-3" 
-                                placeholder="contrasenia" 
-                                aria-label="Recipient's username" 
-                                aria-describedby="button-addon2"
-                                style={{background:'transparent',border:'2px solid var(--button-color)'}} 
-                                />
-                                <button class="btn btn-outline-success" type="button" id="button-addon2" style={{border:'2px solid var(--button-color)',color:'var(--button-color)'}}><i class="fa-solid fa-eye"></i></button>
-                            </div>
-                            <div className="d-grid">
-                                <button className="btn btn-outline-success p-2" style={{border:'2px solid var(--button-color)',color:'var(--button-color)'}}>
-                                    Registrarse
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="container mt-3">
-                        <div style={{textAlign:'center'}}>
-                            <p>o registrate con:</p>
-
-                        </div>
-                        <div className="d-flex gap-3 justify-content-center">
-                            <button className="btn btn-light p-2">
-                                <img src="https://cdn-icons-png.flaticon.com/512/174/174848.png" alt="facebook-logo" style={{width:'30px'}}/>
-                            </button>
-                            <button className="btn btn-light p-2">
-                                <img src="https://cdn-icons-png.flaticon.com/512/300/300221.png" alt="facebook-logo" style={{width:'30px'}}/>
-                            </button>
-                            <button className="btn btn-light p-2">
-                                <img src="https://cdn-icons-png.flaticon.com/512/0/747.png" alt="facebook-logo" style={{width:'30px'}}/>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div>
-
-                </div>
-            </div>
-=======
   return (
     <div
       className="container"
@@ -102,7 +26,6 @@ const Register = () => {
             Registrate, o si ya tienes una cuenta{" "}
             <Link to={"/login"}>Inicia sesion</Link>
           </p>
->>>>>>> ccde0ab430b34ea1db7fbf867300fc84fb1ba549
         </div>
         <div className="container mt-5">
           <form action="" className="d-flex flex-column gap-3">
